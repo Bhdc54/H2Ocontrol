da25b60692582fc39253b1818320ccf031337e6a
Brunno Camargo <hbrunno54@gmail.com>
2025-04-29 19:09:15 -0400

    Seu commit

--- firebase.py
+++ firebase.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Substitua pelo caminho correto do seu JSON
-cred = credentials.Certificate("C:\Users\hbrun\OneDrive\Documentos\h2ocontrol\api\sistemademonitoramento-32fb9-firebase-adminsdk-fbsvc-07985cba11.json")
+cred = credentials.Certificate("C:\Users\hbrun\H2OControl\firebase_config.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
